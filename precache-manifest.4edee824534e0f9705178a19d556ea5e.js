7c600ee25ff86849ea9ad561b3996df0823f6af0
Elixir Kook <justkook@gmail.com>
2018-10-14 21:21:19 +0900

    Updates

---
+++ precache-manifest.4edee824534e0f9705178a19d556ea5e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f9521ac5b62e87334b5b",
+    "url": "/movie_app/static/js/runtime~main.f9521ac5.js"
+  },
+  {
+    "revision": "5347c606c95c2702381e",
+    "url": "/movie_app/static/js/main.5347c606.chunk.js"
+  },
+  {
+    "revision": "d931867c60fd0f725508",
+    "url": "/movie_app/static/js/1.d931867c.chunk.js"
+  },
+  {
+    "revision": "5347c606c95c2702381e",
+    "url": "/movie_app/static/css/main.cda487b5.chunk.css"
+  },
+  {
+    "revision": "09589b4408575886403770f28a4d5312",
+    "url": "/movie_app/index.html"
+  }
+];
